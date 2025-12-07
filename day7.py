4f97ab05c72ddafb23b6b262e9b9f29d68e0b2e1
er2rk <efe@erturk.co>
2025-12-07 10:25:04 +0300

    day7 part 1 done.

---
+++ day7.py
@@ -0,0 +1,63 @@
+lines = []
+with open("day7input") as data:
+    for line in data:
+        line = line.strip()
+        lines.append(line)
+
+#141
+length = len(lines[0])
+#70, right in the middle.
+starting_position = 0
+for i in range(length):
+    if lines[0][i]=="S":
+        starting_position = i
+        break
+
+#Could work with lists and check for duplicates but python's sets eliminates this issue.
+beams = set()
+beams.add(starting_position)
+
+#Check one line.
+def helper(beams, string):
+    new_beams = set()
+    splitter_count = 0
+    for beam in beams:
+        #first check if the beam is in the right range
+        if 0 <= beam < len(string):
+            #if it hits a beam split it
+            if string[beam] == "^":
+                splitter_count += 1
+                new_beams.add(beam-1)
+                new_beams.add(beam+1)
+            #if not let it continue.
+            else:
+                new_beams.add(beam)  
+    return new_beams, splitter_count
+
+#We don't have to check every line, start at index 2 then increment by 2 until index > length-1
+i = 2
+tree_length = len(lines)
+hit_splitter = 0
+
+while i < tree_length-1:
+    #just a simple illustration for myself
+    pretty_line = ""
+    for x in range(tree_length-1):
+        if x in beams:
+            pretty_line += "|"
+        else:
+            pretty_line +=" "
+    print(pretty_line)
+    #the actual counter
+    new_beams, splitter_count = helper(beams, lines[i])
+    beams = new_beams
+    hit_splitter += splitter_count
+    i += 2
+
+print(hit_splitter)
+
+
+
+
+
+
