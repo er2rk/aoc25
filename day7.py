a4d33141b10b9b3d1c5aa5a188e632ae02cd5a22
er2rk <efe@erturk.co>
2025-12-07 11:34:58 +0300

    day7 part 2 added.

--- day7.py
+++ day7.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 lines = []
 with open("day7input") as data:
     for line in data:
@@ -38,7 +40,6 @@ def helper(beams, string):
 i = 2
 tree_length = len(lines)
 hit_splitter = 0
-
 while i < tree_length-1:
     #just a simple illustration for myself
     pretty_line = ""
@@ -46,15 +47,44 @@ while i < tree_length-1:
         if x in beams:
             pretty_line += "|"
         else:
-            pretty_line +=" "
+            pretty_line +="."
     print(pretty_line)
-    #the actual counter
-    new_beams, splitter_count = helper(beams, lines[i])
+    #the actual counter part 1
+    new_beams, splitter_count= helper(beams, lines[i])
     beams = new_beams
     hit_splitter += splitter_count
     i += 2
 
 print(hit_splitter)
+
+#Trying to find the easiest solution for part 2.
+#Keeping the amount of timelines at each position in the string
+timelines = defaultdict(int)
+timelines[starting_position] = 1
+
+#resetting i for the loop
+i = 2
+
+#Same loop structure as part 1.
+while i < tree_length:
+    next_timelines = defaultdict(int)
+    
+    # Iterate through all positions where timelines currently exist
+    for pos, count in timelines.items():
+        if 0 <= pos < len(lines[i]):
+            char = lines[i][pos]   
+            if char == "^":
+                next_timelines[pos - 1] += count
+                next_timelines[pos + 1] += count
+            else:
+                next_timelines[pos] += count
+
+    timelines = next_timelines
+    i += 2
+
+total_timelines = sum(timelines.values())
+print(total_timelines)
+
 
 
 
